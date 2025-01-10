0466cb9e42165b7bc684d9090a6ae0af81f4d2fb
Anjali Sharma <122967990+ItsAnjaliSharma@users.noreply.github.com>
2025-01-10 04:53:36 +0000

    add changes on sidebar

--- next-dashboard/app/ui/dashboard/sidebar/sidebar.jsx
+++ next-dashboard/app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,6 +1,75 @@
+import styles from "./sidebar.module.css"
+import {LayoutDashboard, Building2 } from "lucide-react";
+
+const menuItems = [
+     {
+      label: "Dashboard",
+      path: "/", 
+      icon: <LayoutDashboard />
+    },
+    { title:"Setting",
+        list:[
+    {
+      label: "Company Master",
+      path: "companyMaster",
+      icon:<Building2 />
+    },
+    {
+      label: "Branch Master",
+      path: "branchMaster"
+    },
+
+    {
+      label: "Department Master",
+      path: "deptMaster"
+    },
+    {
+
+      label: "Designation Master",
+      path: "designMaster"
+    },
+    {
+    
+      label: "Role master",
+      path: "userRole"
+    },
+    {
+      label: "Menu Master",
+      path: "menuMaster"
+    },
+
+    {
+      label: "Role permission",
+      path: "roleperMaster"
+    },
+    {
+  
+      label: "Password Master",
+      path: "createPassword"
+    },
+]},
+    { title:"User",
+        list:[
+    {
+      label: "User master",
+      path: "userMaster"
+    },
+    {
+      label: "User Permission",
+      path: "userperMaster"
+    },
+]}
+  ]
+
 const Sidebar=()=>{
     return(
-        <h1>Sidebar</h1>
+
+        <div className={styles.container}>
+            <ul>
+            {menuItems.map(cat=>{
+            <li key={cat.title}>cat.title</li>
+        })}
+        </ul></div>
     )
 
 }
